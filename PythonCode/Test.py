2f3856aaf64101ab72795f24252119e017f3e40b
sophiealtmann <s.altmann@mci4me.at>
2022-12-07 14:19:04 +0100

    Funktioin zum Einlesen der Daten

--- PythonCode/Test.py
+++ PythonCode/Test.py
@@ -3,31 +3,32 @@ from scipy import fftpack
 from scipy import signal
 import matplotlib.pyplot as plt
 import pandas as pd
+import Lab4Functions as l4f
 import csv
 
-data= pd.read_csv('./Data/validierung_beidseitig.txt',   # Bei Validierung: A0 --> links, A --> rechts
-                sep=';',
-                header=None,
-                names=["links",
-                "rechts",
-                "t"])
+# data= pd.read_csv('./Data/validierung_beidseitig.txt',   # Bei Validierung: A0 --> links, A --> rechts
+#                 sep=';',
+#                 header=None,
+#                 names=["links",
+#                 "rechts",
+#                 "t"])
 
     
-plt.plot(data.t,data.links,label='links')
-plt.plot(data.t,data.rechts,label='rechts')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# plt.plot(data.t,data.links,label='links')
+# plt.plot(data.t,data.rechts,label='rechts')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
 
 
-data= pd.read_csv('./Data/V5.txt',                  # Bei Experiment: A0 --> rechts, A1 --> links
-                sep=';',
-                header=None,
-                names=["rechts",
-                "links",
-                "t"])
+# data= pd.read_csv('./Data/V5.txt',                  # Bei Experiment: A0 --> rechts, A1 --> links
+#                 sep=';',
+#                 header=None,
+#                 names=["rechts",
+#                 "links",
+#                 "t"])
 
-    
-plt.plot(data.t,data.links,label='links')
-plt.plot(data.t,data.rechts,label='rechts')
+verena_data= l4f.import_data('verena',';')   
+plt.plot(verena_data.t,verena_data.links,label='links')
+plt.plot(verena_data.t,verena_data.rechts,label='rechts')
 plt.legend(loc="best",frameon=True)
 plt.show()

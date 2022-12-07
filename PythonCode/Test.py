b46cd3d1624997dce483ce781f28ac4e42141ce4
sophiealtmann <s.altmann@mci4me.at>
2022-12-07 10:56:47 +0100

    Eingänge notiert

--- PythonCode/Test.py
+++ PythonCode/Test.py
@@ -5,7 +5,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import csv
 
-data= pd.read_csv('./Data/V5.txt',
+data= pd.read_csv('./Data/validierung_beidseitig.txt',   # Bei Validierung: A0 --> links, A --> rechts
+                sep=';',
+                header=None,
+                names=["links",
+                "rechts",
+                "t"])
+
+    
+plt.plot(data.t,data.links,label='links')
+plt.plot(data.t,data.rechts,label='rechts')
+plt.legend(loc="best",frameon=True)
+plt.show()
+
+
+data= pd.read_csv('./Data/V5.txt',                  # Bei Experiment: A0 --> rechts, A1 --> links
                 sep=';',
                 header=None,
                 names=["rechts",
